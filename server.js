d286b470256012086d4af6141a22a0cb10697f90
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-01-31 09:52:06 +0530

    installed jsonwebtoken, dotenv, bcryptjs, mongoose. And write the user schema, logic of authentication

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({path:"./config.env"});
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+const DB= process.env.DBURI.replace("<password>",process.env.DBPASSWORD);
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedToplogy:true,
+}).then((con)=>{
+    console.log("DB connection is successful");
+}).catch((err)=>{
+    console.log(err);
+});
 
 const port = process.env.PORT || 8000;
 
-server.listen(port,()=>{
-    console.log(`App is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
