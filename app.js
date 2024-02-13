c9f7235067533396c5211ed1b45052ee6e617f88
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-02-14 03:11:52 +0530

    uninstall xss and install xss-clean

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const bodyParser = require("body-parser");
-const xss = require("xss");
+const xss = require("xss-clean");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes/index");
 const cookieParser = require("cookie-parser");
+const session = require("cookie-session");
 
 app.use(
   express.urlencoded({
@@ -16,8 +17,6 @@ app.use(
   })
 );
 
-app.use(mongoSanitize());
-//   app.use(xss());
 app.use(
   cors({
     origin: "*",
@@ -26,6 +25,8 @@ app.use(
   })
 );
 
+app.use(mongoSanitize());
+app.use(xss());
 app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,6 +36,18 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    proxy: true,
+    resave: true,
+    saveUnintialized: true,
+    cookie: {
+      secure: false,
+    },
+  })
+);
 
 const limiter = rateLimit({
   max: 3000,
@@ -54,10 +67,6 @@ module.exports = app;
 
 // express-generator
 // what is the use of views in this folder structure
-// express.Router
-// app.route()
-// Route parameters
-// Route handlers
 // protected route and also check in luster backend repo
 // learn how to make sending file in postman
 // virtual fields in schema
